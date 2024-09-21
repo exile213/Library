12f132d6bf340910893babfaf26950f07823561e
exile213 <exilegamer3@gmail.com>
2024-09-21 10:14:52 +0800

    Add js Add book to library Function
    Update and Fixed js . html classes missmatch

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let hiddenPanel = document.getElementById("bg-form-container");
 let newBookButton = document.getElementById("new-button");
 let cancelButton = document.getElementById("cancel-button");
+let submitButton = document.getElementById("submit-button");
+
+
+//form inputs 
+let bookTitle = document.getElementById("booktitle");
+let bookAuthor = document.getElementById("bookauthor");
+let noOfPages = document.getElementById("pageno");
+let didRead = document.getElementById("isread");
 
 newBookButton.addEventListener("click",function(){
    hiddenPanel.style.visibility = "visible";
@@ -10,23 +18,37 @@ cancelButton.addEventListener("click",function(){
   hiddenPanel.style.visibility = "hidden";
 });
 
+submitButton.addEventListener("click",function(){
+  addBookToLibrary();
+  //displayBooks();
+  event.preventDefault();
+});
+
 
 
 
 
 //ODIN PROJECT LOGIC
-const myLibrary = [];
+let myLibrary = [];
 
+
+//-------------------------------------------
 //constructor
 function book(title,author,pages,isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
-    }
 }
+
 function addBookToLibrary() {
-  // do stuff here
+  newBook = new book(bookTitle.value,bookAuthor.value,noOfPages.value,didRead.checked);
+  myLibrary.push(newBook);
+  console.log(myLibrary[0].title);
+}
+
+function displayBooks(){
+    for(let i = 0; i<=Array.length-1;i++){
+        
+    }
 }
